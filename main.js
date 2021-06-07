77113a7fc661f6ec0ddf9d6509f1e7a34fdaf021
9jiya <74593564+9jiya@users.noreply.github.com>
2021-06-07 07:31:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bed(){
+    window.location="index1.html";
+}
+function tv(){
+    window.location="index2.html";
+}
+function desk(){
+    window.location="index3.html";
+}
+function swing(){
+    window.location="index4.html";
+}
+function cup(){
+    window.location="index5.html";
+}
